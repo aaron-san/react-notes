5e355c631dd4b18f04c9226a2f3e350b8a843bad
fincoder <aaronhardy6@gmail.com>
2022-11-17 21:23:50 +0900

    deleted everything

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const container: HTMLElement = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
-root.render(<App />);
